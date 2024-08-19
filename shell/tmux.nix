39d8c4ccd984afb7b2b72c9c388cdb5e54654d45
Jakub Waszkiewicz <->
2024-08-19 23:39:15 +0200

    configure tmux dracula

--- shell/tmux.nix
+++ shell/tmux.nix
@@ -6,16 +6,16 @@
         mouse = false;
         clock24 = true;
         plugins = with pkgs.tmuxPlugins; [
-          yank
-          sensible
-          {
-            plugin = dracula;
-            extraConfig = ''
-              set -g @dracula-show-battery false
-              set -g @dracula-show-powerline true
-              set -g @dracula-refresh-rate 10
-            '';
-          }
+            yank
+            sensible
+            {
+                plugin = dracula;
+                extraConfig = ''
+                    set -g @dracula-plugins "ssh-session cpu-usage ram-usage"
+                    set -g @dracula-show-powerline true
+                    set -g @dracula-refresh-rate 5
+                '';
+            }
         ];
         extraConfig = ''
         '';
