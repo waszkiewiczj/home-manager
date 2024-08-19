c30dedf4ded60e7f71ffaab4c5ab8a86415f7d71
Jakub Waszkiewicz <->
2024-08-19 23:46:36 +0200

    move zsh setup to separate file

--- shell/default.nix
+++ shell/default.nix
@@ -2,6 +2,7 @@
 
 {
     imports = [
+        ./zsh.nix
         ./vim.nix
         ./tmux.nix
     ];
@@ -10,43 +11,6 @@
         pkgs.tig
         pkgs.k9s
     ];
-
-    programs.zsh = {
-        enable = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-        plugins = [
-            {
-                name = "powerlevel10k";
-                src = pkgs.zsh-powerlevel10k;
-                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-            }
-            {
-                name = "powerlevel10k-config";
-                src = lib.cleanSource ./p10k-config;
-                file = "p10k.zsh";
-            }
-        ];
-        envExtra = ''
-            export ZSH_TMUX_AUTOSTART="true"
-            export ZSH_TMUX_AUTOCONNECT="false"
-        '';
-        oh-my-zsh = {
-            enable = true;
-            plugins = [
-                "tmux"
-                "fzf"
-                "direnv"
-                "git"
-                "docker"
-                "docker-compose"
-                "kubectl"
-                "ansible"
-                "terraform"
-                "gcloud"
-            ];
-        };
-    };
 
     programs.direnv = {
         enable = true;
---
+++ shell/zsh.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+
+{
+    programs.zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        plugins = [
+            {
+                name = "powerlevel10k";
+                src = pkgs.zsh-powerlevel10k;
+                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+            }
+            {
+                name = "powerlevel10k-config";
+                src = lib.cleanSource ./p10k-config;
+                file = "p10k.zsh";
+            }
+        ];
+        envExtra = ''
+            export ZSH_TMUX_AUTOSTART="true"
+            export ZSH_TMUX_AUTOCONNECT="false"
+        '';
+        oh-my-zsh = {
+            enable = true;
+            plugins = [
+                "tmux"
+                "fzf"
+                "direnv"
+                "git"
+                "docker"
+                "docker-compose"
+                "kubectl"
+                "ansible"
+                "terraform"
+                "gcloud"
+            ];
+        };
+    };
+}
