efb1b85eb62306095aeadde00aa67c78f84c49d7
Jakub Waszkiewicz <>
2024-09-19 14:53:24 +0200

    add alias-tips for zsh

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -16,6 +16,15 @@
                 src = lib.cleanSource ./p10k-config;
                 file = "p10k.zsh";
             }
+            {
+                name = "alias-tips";
+                src = pkgs.fetchFromGitHub {
+                    owner = "djui";
+                    repo = "alias-tips";
+                    rev = "41cb143ccc3b8cc444bf20257276cb43275f65c4";
+                    sha256 = "sha256-ZFWrwcwwwSYP5d8k7Lr/hL3WKAZmgn51Q9hYL3bq9vE=";
+                };
+            }
         ];
         envExtra = ''
             export ZSH_TMUX_AUTOSTART="true"
