a0187123e43870db78b6283cf9736b365878bd58
Jakub Waszkiewicz <>
2024-11-03 12:50:37 +0100

    enable fzf-tmux integration

--- shell/fzf.nix
+++ shell/fzf.nix
@@ -4,6 +4,7 @@
     programs.fzf = {
         enable = true;
         enableZshIntegration = true;
+        tmux.enableShellIntegration = true;
     };
 
     programs.vim.plugins = with pkgs.vimPlugins; [
