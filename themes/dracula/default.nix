d672748c0f589be2147f821b633589a4f79f528a
Jakub Waszkiewicz <>
2024-11-03 12:50:51 +0100

    add dracula theme to fzf

--- themes/dracula/default.nix
+++ themes/dracula/default.nix
@@ -39,6 +39,22 @@
         theme = "Dracula";
     };
 
+    # fzf
+    programs.fzf.colors = {
+        fg = "#f8f8f2";
+        "fg+" = "#f8f8f2";
+        # bg = "#282a36"; # comment out to disable the background color (use shell bg instead)
+        "bg+" = "#44475a";
+        hl = "#bd93f9";
+        "hl+" = "#bd93f9";
+        info = "#ffb86c";
+        prompt = "#50fa7b";
+        pointer = "#ff79c6";
+        marker = "#ff79c6";
+        spinner = "#ffb86c";
+        header = "#6272a4";
+    };
+
     # zsh
     home.file.dracula-zsh-theme = {
         enable = true;
