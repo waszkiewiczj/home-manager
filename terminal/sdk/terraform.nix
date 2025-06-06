7e58cb67455ca16ea0e39f4af681810ef9a64f4f
Jakub Waszkiewicz <>
2025-06-06 15:26:10 +0200

    setup hcl syntax highlight and formatter in vscode

--- terminal/sdk/terraform.nix
+++ terminal/sdk/terraform.nix
@@ -11,7 +11,25 @@
         "terraform"
     ];
 
-    programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
-        hashicorp.terraform
-    ];
+    programs.vscode.profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+            hashicorp.terraform
+            hashicorp.hcl
+            jkillian.custom-local-formatters
+        ];
+        userSettings = {
+            "[hcl]" = {
+                "editor.defaultFormatter" = "jkillian.custom-local-formatters";
+            };
+            "customLocalFormatters.formatters" = [
+                {
+                    # full path needs to be specified as vscode does not recognize nix-installed binaries
+                    "command" = "${pkgs.terraform}/bin/terraform fmt -";
+                    "languages" = [
+                        "hcl"
+                    ];
+                }
+            ];
+        };
+    };
 }
