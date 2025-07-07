5af8d6253b12a926f25fd12583ebaa75d2f8a231
Jakub Waszkiewicz <>
2025-07-07 11:45:38 +0200

    add script for logging into gcloud

--- terminal/sdk/gcloud.nix
+++ terminal/sdk/gcloud.nix
@@ -2,7 +2,33 @@
 
 {
     home.packages = with pkgs; [
-        (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
+        (
+            google-cloud-sdk.withExtraComponents [
+                google-cloud-sdk.components.gke-gcloud-auth-plugin
+            ]
+        )
+        (
+            writeShellApplication {
+                text = ''
+                    #!/usr/bin/env bash
+
+                    echo "Logging into gcloud..."
+
+                    if ! gcloud auth print-access-token --quiet 1>/dev/null; then
+                        echo "No gcloud credentials found, running 'gcloud auth login'"
+                        gcloud auth login
+                    fi
+
+                    if ! gcloud auth application-default print-access-token --quiet 1>/dev/null; then
+                        echo "No gcloud application default credentials found, running 'gcloud auth application-default login'"
+                        gcloud auth application-default login
+                    fi
+
+                    echo "gcloud login completed"
+                '';
+                name = "gcloud-login";
+            }
+        )
     ];
 
     programs.zsh.oh-my-zsh.plugins = [
