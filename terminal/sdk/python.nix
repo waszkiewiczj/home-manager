17e39cff67e44dcacbe27088c0f868c7ef59e9a7
Jakub Waszkiewicz <->
2025-07-28 23:11:33 +0200

    add pyenv-virtualenv

--- terminal/sdk/python.nix
+++ terminal/sdk/python.nix
@@ -3,11 +3,21 @@
 {
     programs.pyenv = {
         enable = true;
-        enableZshIntegration = true;
+        enableZshIntegration = false;
+        package = pkgs.pyenv.overrideAttrs (oldAttrs: {
+            buildInputs = with pkgs; [
+                cacert
+                git
+            ];
+            postInstall =  oldAttrs.postInstall + ''
+                git clone https://github.com/pyenv/pyenv-virtualenv.git $out/plugins/pyenv-virtualenv
+            '';
+        });
     };
 
     programs.zsh.oh-my-zsh.plugins = [
         "python"
+        "pyenv"
     ];
 
     programs.vscode.profiles.default = {
