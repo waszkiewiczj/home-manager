6ff2d926f5b6eb0a8a915eb9ebeef07172dd419c
Jakub Waszkiewicz <->
2025-10-13 14:32:53 +0200

    remove deprecated python config

--- terminal/sdk/python.nix
+++ terminal/sdk/python.nix
@@ -1,17 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-    home.packages = with pkgs; [
-        gcc
-        gnumake
-        zlib
-        libffi
-        readline
-        bzip2
-        openssl
-        ncurses
-    ];
-
     programs.pyenv = {
         enable = true;
         enableZshIntegration = false;
@@ -42,16 +31,6 @@
             export PYENV_ROOT="$HOME/.pyenv"
             export PATH="$PYENV_ROOT/bin:$PATH"
             eval "$(pyenv init --path)"
-        '';
-
-        # https://semyonsinchenko.github.io/ssinchenko/post/using-pyenv-with-nixos/
-        envExtra = ''
-            CPPFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
-            CXXFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
-            CFLAGS="-I${pkgs.openssl.dev}/include";
-            LDFLAGS="-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib";
-            CONFIGURE_OPTS="-with-openssl=${pkgs.openssl.dev}";
-            PYENV_VIRTUALENV_DISABLE_PROMPT="1";
         '';
     };
 
